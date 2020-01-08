aa0187ee0d412a0e55c8ad0c22aacd32cc0b5b9b
ljk <liujk0723@gmail.com>
2020-01-08 14:23:08 +0800

    [Add] 添加命名空间

---
+++ ToolCollection/Classes/Extension/UICollectionView+SK.swift
@@ -0,0 +1,46 @@
+//
+//  UICollectionView.swift
+//  Template
+//
+//  Created by Endless Summer on 2019/8/26.
+//  Copyright © 2019 flow. All rights reserved.
+//
+
+import Foundation
+import DifferenceKit
+
+extension UICollectionView: SKKitCompatible {}
+
+extension String: Differentiable {}
+
+public extension UICollectionView {
+    func reload(oldIDs: [String], newIDs: [String]) {
+        let change = StagedChangeset(source: oldIDs, target: newIDs)
+        
+        self.reload(using: change, setData: { (_) in
+            
+        })
+    }
+}
+
+public extension SKKit where Base == UICollectionView {
+    func indexPathForItem(at view: UIView?) -> IndexPath? {
+        if let cell = self.base.parentCell(for: view) {
+            return self.base.indexPath(for: cell)
+        }
+        return nil
+    }
+    
+}
+
+fileprivate extension UICollectionView {
+    func parentCell(for view: UIView?) -> UICollectionViewCell? {
+        if view?.superview == nil {
+            return nil
+        }
+        if let cell = view?.superview as? UICollectionViewCell {
+            return cell
+        }
+        return self.parentCell(for: view?.superview)
+    }
+}
---
+++ ToolCollection/Classes/SKKit/NameSpace.swift
@@ -0,0 +1,29 @@
+//
+//  NameSpace.swift
+//  ToolCollection
+//
+//  Created by Endless Summer on 2020/1/8.
+//
+
+import Foundation
+
+// 定义泛型类
+public final class SKKit<Base> {
+    public let base: Base
+    public init(_ base: Base) {
+        self.base = base
+    }
+}
+
+// 定义泛型协议
+public protocol SKKitCompatible {
+    associatedtype CompatibleType
+    var sk: CompatibleType { get }
+}
+
+// 协议的扩展
+public extension SKKitCompatible {
+    public var sk: SKKit<Self>{
+        get { return SKKit(self) }
+    }
+}
