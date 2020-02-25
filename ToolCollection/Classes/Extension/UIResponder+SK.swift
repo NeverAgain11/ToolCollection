4dc060db4cb098fe3b410c995c828d2866a5204c
ljk <liujk0723@gmail.com>
2020-02-25 09:00:42 +0800

    [Add] 修改 spec

--- ToolCollection/Classes/Extension/UIResponder+SK.swift
+++ ToolCollection/Classes/Extension/UIResponder+SK.swift
@@ -32,7 +32,7 @@ extension UIResponder {
 public extension CaseIterable where Self: Equatable {
     func next() -> Self {
         let all = Self.allCases
-        let idx = all.index(of: self)!
+        let idx = all.firstIndex(of: self)!
         let next = all.index(after: idx)
         return all[next == all.endIndex ? all.startIndex : next]
     }
