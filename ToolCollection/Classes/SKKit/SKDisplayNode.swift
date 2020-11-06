af2aebd17ece36fef6fcc1640655e4a1b6446816
ljk <liujk0723@gmail.com>
2020-11-06 17:18:11 +0800

    添加 Base

--- ToolCollection/Classes/SKKit/SKDisplayNode.swift
+++ ToolCollection/Classes/SKKit/SKDisplayNode.swift
@@ -48,6 +48,44 @@ open class BaseControlNode: ASControlNode {
     
 }
 
+open class BaseButtonNode: ASButtonNode {
+    
+    public typealias BaseControlAction = (_ sender: BaseButtonNode) -> Void
+    
+    open var action: BaseControlAction?
+    
+    open override func didLoad() {
+        super.didLoad()
+        #if DEBUG
+        queue.async { [weak self] in
+            guard let self = self else { return }
+            let typeName = _typeName(type(of: self))
+            DispatchQueue.main.async {
+                guard self.accessibilityIdentifier == nil else { return }
+                self.accessibilityIdentifier = typeName
+            }
+        }
+        #endif
+    }
+    
+    public override init() {
+        super.init()
+        
+        automaticallyManagesSubnodes = true
+        
+        addTarget(self, action: #selector(BaseButtonNode.didPressed(_:)), forControlEvents: .touchUpInside)
+    }
+    
+    open func addAction(_ action: @escaping BaseControlAction) {
+        self.action = action
+    }
+    
+    @objc open func didPressed(_ sender: BaseButtonNode) {
+        action?(sender)
+    }
+    
+}
+
 public struct Carbon {
     public struct Spacing: IdentifiableComponent, Hashable {
         public var height: CGFloat
