267d1cba472756487c1bec3c57f26a4fcc34740e
ljk <liujk0723@gmail.com>
2020-11-26 11:49:57 +0800

    删除 AloeStackView

--- ToolCollection/Classes/Extension/UIResponder+SK.swift
+++ ToolCollection/Classes/Extension/UIResponder+SK.swift
@@ -7,28 +7,28 @@
 
 import Foundation
 
-extension UIResponder {
-    @objc open func responseEvent(_ event: Event) {
-        self.next?.responseEvent(event)
-    }
-    
-    public class Event: NSObject {
-        public init(sender: UIView, identifier: Any) {
-            self.sender = sender
-            self.identifier = identifier
-        }
-        
-        public let sender: UIView
-        public let identifier: Any
-        public var info: [String : Any] = [:]
-    }
-}
-
-public extension CaseIterable where Self: Equatable {
-    func next() -> Self {
-        let all = Self.allCases
-        let idx = all.firstIndex(of: self)!
-        let next = all.index(after: idx)
-        return all[next == all.endIndex ? all.startIndex : next]
-    }
-}
+//extension UIResponder {
+//    @objc open func responseEvent(_ event: Event) {
+//        self.next?.responseEvent(event)
+//    }
+//
+//    public class Event: NSObject {
+//        public init(sender: UIView, identifier: Any) {
+//            self.sender = sender
+//            self.identifier = identifier
+//        }
+//
+//        public let sender: UIView
+//        public let identifier: Any
+//        public var info: [String : Any] = [:]
+//    }
+//}
+//
+//public extension CaseIterable where Self: Equatable {
+//    func next() -> Self {
+//        let all = Self.allCases
+//        let idx = all.firstIndex(of: self)!
+//        let next = all.index(after: idx)
+//        return all[next == all.endIndex ? all.startIndex : next]
+//    }
+//}
